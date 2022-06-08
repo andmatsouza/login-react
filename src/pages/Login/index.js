5b71f776ee2401fa3f51d8f7b4c07f8a0f87bc42
andmatsou <andmatsou@gmail.com>
2022-06-07 21:04:30 -0300

    feat: conectando o front-end com o back-end.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+
+import  api from '../../config/configApi';
 
 export const Login = () => {
+
+    const [user, setUser] = useState({
+      email: '',
+      password: ''
+    });
+
+    const valorInput = e => setUser({...user, [e.target.name]: e.target.value});
+
+    const loginSubmit = async e => {
+      e.preventDefault();
+      //console.log(user.email);
+      const headers = {
+        'Content-Type': 'aplication/json',
+      }
+
+      await api.post("/login", user, {headers})
+      .then((response) => {
+        console.log(response);
+      }).catch((err) => {
+        if(err.response){
+          console.log(err.response);
+        }else{
+          console.log("Erro: tente mais tarde");
+        }
+        
+      });
+    }
+
   return (
     <div>
         <h1>Login</h1>
+        <form onSubmit={loginSubmit}>
+          <label>Usuário: </label>
+          <input type="text" name="email" placeholder="Digite o e-mail" onChange={valorInput} /><br /><br />
+
+          <label>Senha: </label>
+          <input type="password" name="password" placeholder="Digite a senha" onChange={valorInput} /><br /><br />
+
+          <button type="submit">Acessar</button>
+
+        </form>
     </div>
   );
 };
