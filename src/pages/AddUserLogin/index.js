cb6b94bb41e77e0db22823fec9ee7d04ab245344
andmatsou <andmatsou@gmail.com>
2022-06-23 16:14:42 -0300

    feat: adicionado integração do layout da página cadastro do usuário.

--- src/pages/AddUserLogin/index.js
+++ src/pages/AddUserLogin/index.js
@@ -1,116 +1,159 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
-import * as yup from 'yup';
-import api from '../../config/configApi';
+import * as yup from "yup";
+import api from "../../config/configApi";
 
 export const AddUserLogin = () => {
-  
   const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: ''
+    name: "",
+    email: "",
+    password: "",
   });
 
   const [status, setStatus] = useState({
-    type: '',
-    mensagem: ''
+    type: "",
+    mensagem: "",
   });
 
-  const valueInput = e => setUser({...user, [e.target.name]: e.target.value});
+  const valueInput = (e) =>
+    setUser({ ...user, [e.target.name]: e.target.value });
 
-  const addUser = async e => {
+  const addUser = async (e) => {
     e.preventDefault();
 
     if (!(await validate())) return;
-   
 
     const headers = {
-      'headers': {
-        'Content-Type': 'application/json',
-        'Authrization': 'Bearer ' + localStorage.getItem('token')
-      }
-    }
+      headers: {
+        "Content-Type": "application/json",
+        Authrization: "Bearer " + localStorage.getItem("token"),
+      },
+    };
 
-    await api.post('/user', user, headers)
-    .then((response) => {          
+    await api
+      .post("/user", user, headers)
+      .then((response) => {
         setStatus({
-          type: 'success',
-          mensagem: response.data.mensagem 
+          type: "success",
+          mensagem: response.data.mensagem,
         });
-    }).catch((err) => {
-      if (err.response.data.erro) {        
-        setStatus({
-          type: 'error',
-          mensagem: err.response.data.mensagem 
-        });
-      }else{          
-        setStatus({
-          type: 'error',
-          mensagem: "Erro: Tente mais tarde!" 
-        });
-      }
-    })
-  }
+      })
+      .catch((err) => {
+        if (err.response.data.erro) {
+          setStatus({
+            type: "error",
+            mensagem: err.response.data.mensagem,
+          });
+        } else {
+          setStatus({
+            type: "error",
+            mensagem: "Erro: Tente mais tarde!",
+          });
+        }
+      });
+  };
 
   async function validate() {
     let schema = yup.object({
-      password: yup.string("Erro: Necessário preencher o campo senha!")
-      .required("Erro: Necessário preencher o campo senha!")
-      .min(6,"Erro: A senha deve ter no mínimo 6 caracteres!"),
-      email: yup.string("Erro: Necessário preencher o campo e-mail!")
-      .email("Erro: Necessário preencher o campo e-mail!")
-      .required("Erro: Necessário preencher o campo e-mail!"),
-      name: yup.string("Erro: Necessário preencher o campo nome!")
-      .required("Erro: Necessário preencher o campo nome!")
-      
+      password: yup
+        .string("Erro: Necessário preencher o campo senha!")
+        .required("Erro: Necessário preencher o campo senha!")
+        .min(6, "Erro: A senha deve ter no mínimo 6 caracteres!"),
+      email: yup
+        .string("Erro: Necessário preencher o campo e-mail!")
+        .email("Erro: Necessário preencher o campo e-mail!")
+        .required("Erro: Necessário preencher o campo e-mail!"),
+      name: yup
+        .string("Erro: Necessário preencher o campo nome!")
+        .required("Erro: Necessário preencher o campo nome!"),
     });
-    
-  try {
-    await schema.validate({
-      name: user.name,
-      email: user.email,
-      password: user.password,
-    });
-    return true;
-} catch (err) {      
-    setStatus({
-      type: 'error',
-      mensagem: err.errors 
-    });
-    return false;
-}
+
+    try {
+      await schema.validate({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      });
+      return true;
+    } catch (err) {
+      setStatus({
+        type: "error",
+        mensagem: err.errors,
+      });
+      return false;
+    }
   }
 
   return (
-    <div>
-        <h1>Cadastrar Usuário</h1> 
-        
-        {status.type === 'error' ? <p style={{color: "#ff0000"}}>{status.mensagem}</p> : ""}
-      {status.type === 'success' ?
+    <div className="d-flex">
+      <div className="container-login">
+        <div className="wrapper-login">
+          <div className="title">
+            <span>Cadastrar Usuário</span>
+          </div>
+          {status.type === "error" ? (
+            <p style={{ color: "#ff0000" }}>{status.mensagem}</p>
+          ) : (
+            ""
+          )}
+          {status.type === "success" ? (
+            <Navigate
+              to="/"
+              state={{
+                type: "success",
+                mensagem: status.mensagem,
+              }}
+            />
+          ) : (
+            ""
+          )}
+          <hr />
 
-      <Navigate to="/" state={{
-        type: "success",
-        mensagem: status.mensagem
-      }} />
-
-     : ""}
-     <hr /> 
-
-     <form onSubmit={addUser}>
-        <label>Nome*:</label>
-        <input type="text" name="name" placeholder="Nome completo do usuário" onChange={valueInput} /><br /><br />
-
-        <label>E-mail*:</label>
-        <input type="email" name="email" placeholder="Melhor e-mail do usuário" onChange={valueInput} /><br /><br />
-
-        <label>Senha*:</label>
-        <input type="password" name="password" placeholder="Senha para acessar o sistema" autoComplete="on" onChange={valueInput} /><br /><br />
-
-        * Compo obrigatório <br /><br />
-
-        <button type="submit">Cadastrar</button><br /><br />        
-      </form>
-      <Link to="/">Acessar</Link>
+          <form onSubmit={addUser} className="form-login">
+            <div className="row">
+              <label>Nome*</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Nome completo do usuário"
+                onChange={valueInput}
+              />
+            </div>
+            <div className="row">
+              <label>E-mail*</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Melhor e-mail do usuário"
+                onChange={valueInput}
+              />
+            </div>
+            <div className="row">
+              <label>Senha*</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Senha para acessar o sistema"
+                autoComplete="on"
+                onChange={valueInput}
+              />
+            </div>
+            <br /><br />           
+            <span>* Campo obrigatório</span>           
+            <br /><br />
+            <div className="row button">
+              <button type="submit" className="button-login">
+                Cadastrar
+              </button>
+            </div>
+            <div className="signup-link">
+              <Link to="/" className="link-pg-login">
+                Acessar
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
