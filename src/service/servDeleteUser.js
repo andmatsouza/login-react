8d429310eb40c9cfdcc9d0b4b830288df1b69a39
andmatsou <andmatsou@gmail.com>
2022-07-26 16:47:33 -0300

    feat: Adicionado funções p deletar fabricante, veículo e modelo.

--- src/service/servDeleteUser.js
+++ src/service/servDeleteUser.js
@@ -33,3 +33,103 @@ export const servDeleteUser = async (idUser) => {
 
   return status;
 }
+
+export const servDeleteFabricante = async (idFabricante) => {  
+
+  let status = false;
+
+   const headers = {
+      herders: {
+        Authorizaton: "Bearer " + localStorage.getItem("token"),
+      },
+    };
+
+    await api.put("/fabricante/" + idFabricante, headers )
+    .then((response) => {
+      status = {
+        type: "success",
+        mensagem: response.data.mensagem,
+      };
+    }).catch((err) => {
+      if (err.response.data.erro) {        
+        status = {
+          type: "erro",
+          mensagem: err.response.data.mensagem,
+        };
+      } else {
+        status = {
+          type: "erro",
+          mensagem: "Erro: Tente mais tarde!",
+        };
+      }
+    })
+
+  return status;
+}
+
+export const servDeleteVeiculo = async (idVeiculo) => {  
+
+  let status = false;
+
+   const headers = {
+      herders: {
+        Authorizaton: "Bearer " + localStorage.getItem("token"),
+      },
+    };
+    
+    await api.put("/veiculo/" + idVeiculo, headers )
+    .then((response) => {
+      status = {
+        type: "success",
+        mensagem: response.data.mensagem,
+      };
+    }).catch((err) => {
+      if (err.response.data.erro) {        
+        status = {
+          type: "erro",
+          mensagem: err.response.data.mensagem,
+        };
+      } else {
+        status = {
+          type: "erro",
+          mensagem: "Erro: Tente mais tarde!",
+        };
+      }
+    })
+
+  return status;
+}
+
+export const servDeleteModelo = async (idModelo) => {  
+
+  let status = false;
+
+   const headers = {
+      herders: {
+        Authorizaton: "Bearer " + localStorage.getItem("token"),
+      },
+    };    
+
+
+    await api.delete("/modelo/" + idModelo, headers )
+    .then((response) => {
+      status = {
+        type: "success",
+        mensagem: response.data.mensagem,
+      };
+    }).catch((err) => {
+      if (err.response.data.erro) {        
+        status = {
+          type: "erro",
+          mensagem: err.response.data.mensagem,
+        };
+      } else {
+        status = {
+          type: "erro",
+          mensagem: "Erro: Tente mais tarde!",
+        };
+      }
+    })
+
+  return status;
+}
