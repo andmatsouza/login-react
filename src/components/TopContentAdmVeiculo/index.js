819af738a395b3806970f0d80a2f0412169c8093
andmatsou <andmatsou@gmail.com>
2022-08-22 14:17:34 -0300

    feat: Adicionado componente.

---
+++ src/components/TopContentAdmVeiculo/index.js
@@ -0,0 +1,9 @@
+//import { TopContentButton } from "../TopContentButton";
+
+export function TopContentAdmVeiculo(props) {
+  return (
+    <>     
+      <span className="title-content-veiculo">{`${props.veiculo} - ${props.placa}`}</span>     
+    </>
+  );
+}
