2ea213d810f60d3761fc09a61e7d9689990c8aac
andmatsou <andmatsou@gmail.com>
2022-09-12 14:49:59 -0300

    feat: Adicionado pacote dotenv

--- src/classes/Veiculo/index.js
+++
@@ -1,7 +0,0 @@
-export default class Veiculo
- {
-  constructor(placa, totlt){
-    this.veic = [placa];
-    this.litro = [totlt];
-  }
-}
--- src/config/configApi.js
+++ src/config/configApi.js
@@ -2,7 +2,14 @@
 const axios = require('axios').default;
 
 
+/*export default axios.create({
+  baseURL: 'https://enviostelelimp.com.br',
+  headers: {'Content-Type': 'application/json'}
+});*/
+
 export default axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://localhost:3001',
   headers: {'Content-Type': 'application/json'}
 });
+
+
